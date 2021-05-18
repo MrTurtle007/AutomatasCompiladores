b1b4a8ee60b9070f540e94bcb7a040c06771bd08
MrTurtle007 <erik.ceci22@gmail.com>
2021-05-18 10:57:54 -0500

    Autómatas 1 - 5

---
+++ src/automatascompiladores/FuncionesA2.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatascompiladores;
+
+/**
+ *
+ * @author Mr.Turtle007
+ */
+public class FuncionesA2 {
+    
+    String cadena;
+    char[] caracter;
+    int cont = 0;
+    String concatenacion = "";
+    boolean validacion = true;
+
+    public FuncionesA2(String cadena) {
+        this.cadena = cadena;
+        caracter = cadena.toCharArray();
+        
+        S();
+    }
+
+    private void S() {
+        while(cont < cadena.length()){
+            if(caracter[cont] == 'a'){
+                concatenacion = concatenacion + 'a';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                if(cont == cadena.length()){
+                    break;
+                }
+                else{
+                    A();
+                }
+            }
+            else if(caracter[cont] == 'b'){
+                concatenacion = concatenacion + 'b';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                if(cont == cadena.length()){
+                    validacion = false;
+                    break;
+                }
+                else{
+                    S();
+                }
+            }
+            else if(caracter[cont] == 'c'){
+                concatenacion = concatenacion + 'c';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                
+                if(cont == cadena.length()){
+                    validacion = false;
+                    break;
+                }
+                else{
+                    S();
+                }
+            }
+            else{
+                cont = cadena.length();
+                validacion = false;
+            }
+        }
+    }
+
+    private void A() {
+        while(cont < cadena.length()){
+            if(caracter[cont] == 'a'){
+                concatenacion = concatenacion + 'a';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                if(cont == cadena.length()){
+                    break;
+                }
+                else{
+                    A();
+                }
+            }
+            else if(caracter[cont] == 'b'){
+                concatenacion = concatenacion + 'b';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                if(cont == cadena.length()){
+                    break;
+                }
+                else{
+                    A();
+                }
+            }
+            else if(caracter[cont] == 'c'){
+                concatenacion = concatenacion + 'c';
+                System.out.println("Secuencia: " + concatenacion);
+                cont++;
+                
+                if(cont == cadena.length()){
+                    break;
+                }
+                else{
+                    A();
+                }
+            }
+            else{
+                cont = cadena.length();
+                validacion = false;
+            }
+        }
+    }
+
+    public boolean getValidacion(){
+        return validacion;
+    }
+}
